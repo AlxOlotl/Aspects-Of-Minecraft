dc23db95ef4a11668eaf27610220f15dfd69f52f
Alx_Olotl <finalmorningtstar0108@gmail.com>
2025-09-28 09:40:44 -0400

    .

--- src/main/java/net/alex/aspectsofminecraft/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/alex/aspectsofminecraft/datagen/ModBlockTagGenerator.java
@@ -1,8 +1,10 @@
 package net.alex.aspectsofminecraft.datagen;
 
 import net.alex.aspectsofminecraft.Aspects;
+import net.alex.aspectsofminecraft.block.ModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +19,35 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.RAW_COBALT_BLOCK.get(),
+                        ModBlocks.COBALT_BLOCK.get(),
+                        ModBlocks.COBALT_ORE.get(),
+                        ModBlocks.DEEPSLATE_COBALT_ORE.get(),
+                        ModBlocks.BAMBOO_CORAL_BLOCK.get(),
+                        ModBlocks.DEAD_BAMBOO_CORAL_BLOCK.get(),
+                        ModBlocks.SHELF_CORAL_BLOCK.get(),
+                        ModBlocks.DEAD_SHELF_CORAL_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.RAW_COBALT_BLOCK.get(),
+                        ModBlocks.COBALT_BLOCK.get(),
+                        ModBlocks.COBALT_ORE.get(),
+                        ModBlocks.DEEPSLATE_COBALT_ORE.get(),
+                        ModBlocks.BAMBOO_CORAL_BLOCK.get(),
+                        ModBlocks.DEAD_BAMBOO_CORAL_BLOCK.get(),
+                        ModBlocks.SHELF_CORAL_BLOCK.get(),
+                        ModBlocks.DEAD_SHELF_CORAL_BLOCK.get(),
+                        ModBlocks.HAG_GOO_BLOCK.get());
+
+        this.tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(ModBlocks.BOEBO_PLANKS.get(),
+                        ModBlocks.BOEBO_WOOD.get(),
+                        ModBlocks.STRIPPED_BOEBO_WOOD.get());
+
+        this.tag(BlockTags.MINEABLE_WITH_HOE)
+                .add(ModBlocks.HAG_GOO_BLOCK.get());
     }
 
     @Override
